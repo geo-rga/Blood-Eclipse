1964c77679e3d7787f53fe2b7212746f7f1be537
Georga <georga@3sidedcube.com>
2025-02-21 10:07:34 +0000

    Documentation and removing links to other projects

--- src/test/java/tests/InitClass.java
+++ src/test/java/tests/InitClass.java
@@ -21,20 +21,26 @@ public class InitClass {
 	@BeforeMethod
 	public void beforeMethod() {
 		try {
+			// This gets the execution platform from the run config
 			String configPlatform = System.getProperty("platform");
 			System.out.println("Execution target platform: " + configPlatform);
 			
+			// This sets up the device etc. to be run on
 			DesiredCapabilities capabilities = new DesiredCapabilities();
 			
 			if(configPlatform.equalsIgnoreCase("Android")) {
+				// All Android setup is defined in the below AndroidCapabilities util
 				capabilities = AndroidCapabilities.getCapabilities();
 				
 			} else if (configPlatform.equalsIgnoreCase("iOS")) {
+				// All iOS setup is defined in the below IOSCapabilities util
 				capabilities = IOSCapabilities.getCapabilities();
 			} else {
+				// If platform variable isn't provided, exception here is thrown
 				throw new IllegalArgumentException("Invalid platform: " + configPlatform);
 			}
 			
+			// This is the default 
 			URL url = new URL("http://127.0.0.1:4723/");
 			
 			driver = new AppiumDriver(url, capabilities);
